f9857e7b8f279ae9434f89a2acca99e3889fd43b
Michael Y. Qiu <altmickymike@outlook.com>
2026-01-03 22:13:55 +0800

    Initial Commit

---
+++ DynamicNotif/DynamicNotifApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicNotifApp.swift
+//  DynamicNotif
+//
+//  Created by Michael Qiu on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicNotifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
